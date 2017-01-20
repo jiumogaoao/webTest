bd3f4ac9a40d8ac58315a112f6a44f532338bff3
gaozhiqiang <gaozq@zoteri.net>
2017-01-20 17:24:45 +0800

    change
    change

--- MyApplication/app/src/main/java/com/example/gaozhiqiang/myapplication/tool/NetworkTool.java
+++ MyApplication/app/src/main/java/com/example/gaozhiqiang/myapplication/tool/NetworkTool.java
@@ -1,14 +1,20 @@
 package com.example.gaozhiqiang.myapplication.tool;
 
+import android.util.Log;
+
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import static com.example.gaozhiqiang.myapplication.MainActivity.TAG;
 
 /**
  * Created by gaozhiqiang on 2017/1/20.
@@ -100,6 +106,7 @@ public class NetworkTool {
         }
         System.out.println("info:" + url + " download success");
     }
+
     public static byte[] readInputStream(InputStream inputStream) throws IOException {
         byte[] buffer = new byte[1024];
         int len = 0;
@@ -109,6 +116,74 @@ public class NetworkTool {
         }
         bos.close();
         return bos.toByteArray();
+    }
+
+    /* 上传文件至Server的方法 */
+    private void uploadFile()
+    {
+        String end ="\r\n";
+        String twoHyphens ="--";
+        String boundary ="*****";
+        String newName ="image.jpg";
+        String uploadFile ="/sdcard/image.JPG";
+        String actionUrl ="http://192.168.0.71:8086/HelloWord/myForm";
+        try
+        {
+            URL url =new URL(actionUrl);
+            HttpURLConnection con=(HttpURLConnection)url.openConnection();
+          /* 允许Input、Output，不使用Cache */
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.setUseCaches(false);
+          /* 设置传送的method=POST */
+            con.setRequestMethod("POST");
+          /* setRequestProperty */
+            con.setRequestProperty("Connection", "Keep-Alive");
+            con.setRequestProperty("Charset", "UTF-8");
+            con.setRequestProperty("Content-Type",
+                    "multipart/form-data;boundary="+boundary);
+          /* 设置DataOutputStream */
+            DataOutputStream ds =
+                    new DataOutputStream(con.getOutputStream());
+            ds.writeBytes(twoHyphens + boundary + end);
+            ds.writeBytes("Content-Disposition: form-data; "+
+                    "name=\"file1\";filename=\""+
+                    newName +"\""+ end);
+            ds.writeBytes(end);
+          /* 取得文件的FileInputStream */
+            FileInputStream fStream =new FileInputStream(uploadFile);
+          /* 设置每次写入1024bytes */
+            int bufferSize =1024;
+            byte[] buffer =new byte[bufferSize];
+            int length =-1;
+          /* 从文件读取数据至缓冲区 */
+            while((length = fStream.read(buffer)) !=-1)
+            {
+            /* 将资料写入DataOutputStream中 */
+                ds.write(buffer, 0, length);
+            }
+            ds.writeBytes(end);
+            ds.writeBytes(twoHyphens + boundary + twoHyphens + end);
+          /* close streams */
+            fStream.close();
+            ds.flush();
+          /* 取得Response内容 */
+            InputStream is = con.getInputStream();
+            int ch;
+            StringBuffer b =new StringBuffer();
+            while( ( ch = is.read() ) !=-1 )
+            {
+                b.append( (char)ch );
+            }
+          /* 将Response显示于Dialog */
+            Log.d(TAG, "上传成功"+b.toString().trim());
+          /* 关闭DataOutputStream */
+            ds.close();
+        }
+        catch(Exception e)
+        {
+            Log.d(TAG, "uploadFile: "+e);
+        }
     }
 
 }
